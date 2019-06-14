4cb1013679bf866a50bdd19c552ab84436b2d096
ccdle12 <chris.coverdale24@gmail.com>
2019-06-14 15:42:03 +0700

    tests/test_tcp-server.cpp: using smart pointer to reference via interface

--- tests/test_tcp-server.cpp
+++ tests/test_tcp-server.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
 #include "tcp-server/tcp-server.h"
+#include <memory>
 
 TEST(TestServerInit, OutputTest) {
-  TCPServer server = TCPServer(5040);
-  server.Listen();
+    auto server = std::unique_ptr<Server>(new TCPServer(5040));
+    server->Listen();
 }
 
