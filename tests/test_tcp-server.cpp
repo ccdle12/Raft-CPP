38d860cfa0f778c50923632f3e5331c009e2ab97
ccdle12 <chris.coverdale24@gmail.com>
2019-06-15 12:10:54 +0700

    tests/test_tcp-server: fixing basic test

--- tests/test_tcp-server.cpp
+++ tests/test_tcp-server.cpp
@@ -1,10 +1,18 @@
 #include <gtest/gtest.h>
 #include "tcp-server/tcp-server.h"
 #include <memory>
+#include <thread>
+
 
 TEST(TestServerInit, OutputTest) {
-    auto server = std::unique_ptr<Server>(new TCPServer(5040));
-    server->Listen();
-    server->Close();
-}
+    // Run the TCPServer on a background thread.
+    TCPServer server = TCPServer(5040);
+    std::thread t1(&TCPServer::Listen, &server);
+    t1.detach();
 
+    // Sleep to give some time for the thread to run.
+    usleep(500);
+
+    // Force close the server.
+    server.Close();
+}
