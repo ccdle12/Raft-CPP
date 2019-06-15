419770d465dac1ff5c937745a102fd300393ff49
ccdle12 <chris.coverdale24@gmail.com>
2019-06-15 11:06:40 +0700

    src/main.cpp: adding server Listen to main for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include "tcp-server/tcp-server.h"
 
 int main() {
+  TCPServer server = TCPServer(5041);
+  std::cout << "Now running server listen" << std::endl;
+  server.Listen();
   return 0;
 }
