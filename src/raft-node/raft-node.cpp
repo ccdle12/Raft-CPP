6278fa215d2449bb4aa100be6b501ab8f47b0a7a
ccdle12 <chris.coverdale24@gmail.com>
2019-06-18 14:25:09 +0700

    src/raft-node: adding run and send functions

--- src/raft-node/raft-node.cpp
+++ src/raft-node/raft-node.cpp
@@ -2,5 +2,27 @@
 
 void RaftNode::Run() 
 {
-  std::cout << "Hello" << std::endl;
+  std::thread t1(&RaftNode::RunP2PServer, this);
+  t1.detach();
+
+  while(true)
+  {
+    std::cout << "RAFTNODE: Debug printing in while loop\n" << std::endl;
+    sleep(1);
+  }
+}
+
+void RaftNode::RunP2PServer()
+{
+  p2p_server_->Listen();
+}
+
+void RaftNode::SendMsg(const std::string& msg)
+{
+  p2p_client_->Send(msg);
+}
+
+const std::string& RaftNode::ReadP2PBuffer() const
+{
+  return p2p_client_->GetBuffer();
 }
--- src/raft-node/raft-node.h
+++ src/raft-node/raft-node.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 
 #include "tcp-server/tcp-server.h"
 #include "tcp-client/tcp-client.h"
@@ -10,15 +11,21 @@ class RaftNode {
   public:
     RaftNode(std::string node_addr, unsigned int p2p_port) : p2p_port_{p2p_port}, node_addr_{node_addr} 
     {
-      // TEMP!
       p2p_server_ = std::unique_ptr<Server>(new TCPServer(p2p_port_));
       p2p_client_ = std::unique_ptr<Client>(new TCPClient(node_addr_, p2p_port_));
     };
+
     void Run();
+    void SendMsg(const std::string& msg);
+    const std::string& ReadP2PBuffer() const;
+
   private:
     unsigned int p2p_port_;
     std::string node_addr_;
 
     std::unique_ptr<Server> p2p_server_;
     std::unique_ptr<Client> p2p_client_;
+
+    // Internal Methods.
+    void RunP2PServer();
 };
