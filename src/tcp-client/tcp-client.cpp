8edad51af3478091c70dff424260e5dc9db2bb6c
ccdle12 <chris.coverdale24@gmail.com>
2019-08-29 16:55:20 +0100

    src/tcp-client: refacotring socket method

--- src/tcp-client/tcp-client.cpp
+++ src/tcp-client/tcp-client.cpp
@@ -1,20 +1,26 @@
 #include "tcp-client.h"
 
-TCPClient::TCPClient(const std::string& address, unsigned int port, int IPVersion) : port_{port}, address_{address} 
+TCPClient::TCPClient(const std::string& address, unsigned int port, int ip_version) : port_{port}, address_{address} 
 {
-    if (IPVersion != AF_INET && IPVersion != AF_INET6)
+    if (is_an_ipv(ip_version))
         throw std::runtime_error("ip_verison must be either: AF_INET (ipv4) or AF_INET6 (ipv6)");
 
-    IPV_ = IPVersion;
+    IPV_ = ip_version;
 }; 
 
-// Send is the interface implementation of Client. This will send data to a 
+// Checks if a passed ip_version conforms to IPV4 or IPV6.
+bool TCPClient::is_an_ipv(const int ip_version) const
+{
+    return ip_version != AF_INET && ip_version != AF_INET6;
+} 
+
+// Send is the interface implementation of Client. This will send a message to a 
 // specified server.
 void TCPClient::Send(const std::string& msg)
 {
     try 
     {
-      Socket();    
+      create_socket();    
       Bind();
       Connect();
       SendMsg(msg);
@@ -24,12 +30,18 @@ void TCPClient::Send(const std::string& msg)
     }
 }
 
-void TCPClient::Socket()
+// Creates a socket using the standard socket() system call.
+void TCPClient::create_socket()
 {
-  if ( -1 == (sock_fd_ = socket(IPV_, kProtocolType_, 0)))
-  {
-     throw ErrMsg("Unable to create socket\n"); 
-  }
+  socket_file_descriptor_ = socket(IPV_, kProtocolType_, 0);
+  if (!is_socket_open(socket_file_descriptor_))
+      throw std::runtime_error("Failed to create socket connection");
+}
+
+// Checks if a socket was succesfully created.
+bool TCPClient::is_socket_open(const int socket_fd) const 
+{
+    return -1 != socket_fd;
 }
 
 void TCPClient::Bind()
@@ -45,7 +57,7 @@ void TCPClient::Bind()
 
 void TCPClient::Connect()
 {
-  if (connect(sock_fd_, (sockaddr*)&server_addr_, sizeof(server_addr_)))
+  if (connect(socket_file_descriptor_, (sockaddr*)&server_addr_, sizeof(server_addr_)))
   {
     throw ErrMsg("Connection attempt failed\n");
   }
@@ -53,13 +65,13 @@ void TCPClient::Connect()
 
 void TCPClient::SendMsg(const std::string& msg)
 {
-  int sent = send(sock_fd_, &msg, msg.length(), 0);
+  int sent = send(socket_file_descriptor_, &msg, msg.length(), 0);
   if (-1 == sent)
   {
     std::cout << "Failed to send messge\n" << std::endl;
   }
 
-  server_res_ = read(sock_fd_, buffer_, 1024);
+  server_res_ = read(socket_file_descriptor_, buffer_, 1024);
   printf("CLIENT: Received response from server: %s\n", buffer_);
 }
 
@@ -72,5 +84,5 @@ const std::string& TCPClient::GetBuffer() const
 TCPClient::~TCPClient()
 {
   std::cout << "Closing file descriptor" << std::endl;
-  close(sock_fd_);
+  close(socket_file_descriptor_);
 }
