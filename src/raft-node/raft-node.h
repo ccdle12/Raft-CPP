7ad761b9698cefa75b83697a78a67fe549d077c0
ccdle12 <chris.coverdale24@gmail.com>
2019-08-31 13:18:18 +0100

    src/raft-node: refactoring to use uint8_t

--- src/raft-node/raft-node.h
+++ src/raft-node/raft-node.h
@@ -11,19 +11,19 @@ class RaftNode {
   public:
     RaftNode(std::string node_addr, unsigned int p2p_port) : p2p_port_{p2p_port}, node_addr_{node_addr} 
     {
-      p2p_server_ = std::unique_ptr<Server>(new TCPServer(p2p_port_));
+      p2p_server_ = std::unique_ptr<NetworkServer>(new TCPServer(p2p_port_, AF_INET));
       p2p_client_ = std::unique_ptr<NetworkClient>(new TCPClient(node_addr_, p2p_port_, AF_INET));
     };
 
     void Run();
     void SendMsg(const uint8_t msg);
-    const uint8_t* ReadP2PBuffer() const;
+    const uint8_t* read_p2p_buffer() const;
 
   private:
     unsigned int p2p_port_;
     std::string node_addr_;
 
-    std::unique_ptr<Server> p2p_server_;
+    std::unique_ptr<NetworkServer> p2p_server_;
     std::unique_ptr<NetworkClient> p2p_client_;
 
     void RunP2PServer();
