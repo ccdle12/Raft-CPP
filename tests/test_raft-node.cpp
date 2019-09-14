6bb99ff5551dc4dc3814f9f04f214d3cb6223fed
ccdle12 <chris.coverdale24@gmail.com>
2019-09-14 16:03:30 +0100

    tests/test_raft-node: first round of heartbeat tests

--- tests/test_raft-node.cpp
+++ tests/test_raft-node.cpp
@@ -28,3 +28,22 @@ TEST(TestRaftNode, SendSimpleMessage) {
     ASSERT_EQ(*response, 11);
     ASSERT_EQ(*response, 0xB);
 }
+
+TEST(TestRaftNode, HeartBeat) {
+    auto node = RaftNode(LOCAL_HOST, 5043);
+    std::thread t1(&RaftNode::Run, &node);
+    t1.detach();
+
+    auto node_2 = RaftNode(LOCAL_HOST, 5044);
+    std::thread t2(&RaftNode::Run, &node_2);
+    t2.detach();
+
+    // Send a heartbeat message from node_1 to node_2.
+    node.send_heart_beat(LOCAL_HOST, 5044);
+
+    // Read the response in the buffer. It should be a 'Ack': 0x02.
+    const uint8_t* ack = node.read_p2p_buffer();
+    printf("Ack: %X\n", *ack);
+
+    ASSERT_EQ(*ack, 0x02);
+}
