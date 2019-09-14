c53185acc5ea86a774f035ecaf99356acf448cd4
ccdle12 <chris.coverdale24@gmail.com>
2019-09-14 16:02:30 +0100

    src/raft-node/raft-node: first round of heartbeat and ServerEventListener

--- src/raft-node/raft-node.h
+++ src/raft-node/raft-node.h
@@ -3,21 +3,31 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <thread>
 #include "tcp-client/tcp-client.h"
 #include "tcp-server/tcp-server.h"
-#include <thread>
+#include "server_event_listener.h"
 
-class RaftNode {
+#define HEART_BEAT_MSG 0x01
+#define ACK 0x02
+
+class RaftNode : public ServerEventListener {
   public:
     RaftNode(std::string node_addr, unsigned int p2p_port) : p2p_port_{p2p_port}, node_addr_{node_addr} 
     {
-      p2p_server_ = std::unique_ptr<NetworkServer>(new TCPServer(p2p_port_, AF_INET));
+      p2p_server_ = std::unique_ptr<NetworkServer>(new TCPServer(p2p_port_, AF_INET, *this));
       p2p_client_ = std::unique_ptr<NetworkClient>(new TCPClient(node_addr_, p2p_port_, AF_INET));
     };
 
     void Run();
     void SendMsg(const uint8_t msg);
     const uint8_t* read_p2p_buffer() const;
+
+    // HeartBeat.
+    void send_heart_beat(const std::string local_host, const int port);
+
+    // Event Listener Implementation.
+    void MsgReceived() const override;
 
   private:
     unsigned int p2p_port_;
